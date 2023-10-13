ded0c4dfbe6ec07a7a2fed31fb554624e782a073
gaialorefice <gaialorefice.gl@gmail.com>
2023-10-13 12:57:22 +0200

    comments model

--- models/Comment.js
+++ models/Comment.js
@@ -18,4 +18,4 @@ const PostSchema = new mongoose.Schema({
     {timestamps: true}, 
 );
 
-module.exports = mongoose.model("Post",PostSchema);
+module.exports = mongoose.model("Comment",CommSchema);
